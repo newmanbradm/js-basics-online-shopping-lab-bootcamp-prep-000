74cf798205a619234eede243310568500a46b7ad
Brad Newman <newmanbradm@gmail.com>
2018-03-18 18:47:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   if(cardNumber === undefined) {
     return "sorry, we don't have a credit card on file for you."
+  } else {
+    
   }
-  
 }
